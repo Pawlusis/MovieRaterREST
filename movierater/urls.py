1e6cf8b648a93bb66655edcfee2d5b0f984e8ff3
Pawlusis <pawlusis12@gmail.com>
2023-02-02 20:39:25 +0100

    Done RestApi with Auth&Perm with Code reformat

--- movierater/urls.py
+++ movierater/urls.py
@@ -2,9 +2,10 @@ from django.urls import include
 from django.contrib import admin
 from django.urls import path
 from api import urls
-
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(urls)),
+    path('auth/', views.obtain_auth_token)
 ]
