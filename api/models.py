b55c28c73be67b885d2974dcdede2368bb4b6a45
Pawlusis <pawlusis12@gmail.com>
2023-02-02 01:32:29 +0100

    fixies

--- api/models.py
+++ api/models.py
@@ -4,3 +4,8 @@ class Movie(models.Model):
     tittle = models.CharField(max_length=64)
     describe = models.TextField(max_length=256)
     after_premiere = models.BooleanField(default=False)
+    premiere = models.DateField(null=True, blank=True)
+    year = models.IntegerField()
+    imdb_rating = models.DecimalField(max_digits=4, decimal_places=2)
+
+
